c857f27ff14dab00112d956b5349d02368e42ddd
nazli-cam <nazli@e2.vc>
2026-06-06 23:07:22 +0300

    Update csrf.js

--- src/middleware/csrf.js
+++ src/middleware/csrf.js
@@ -1,6 +1,16 @@
 const csurf = require('csurf');
 
-const csrfProtection = csurf({ cookie: false });
+const csrfProtection = csurf({
+  cookie: false,
+  value: (req) =>
+    req.headers['csrf-token'] ||      // ← frontend sends 'CSRF-Token'
+    req.headers['x-csrf-token'] ||
+    req.headers['xsrf-token'] ||
+    req.headers['x-xsrf-token'] ||
+    (req.body && req.body._csrf) ||   // ← HTML form hidden input
+    (req.query && req.query._csrf) ||
+    ''
+});
 
 function csrfToken(req, res, next) {
   res.locals.csrfToken = req.csrfToken();
