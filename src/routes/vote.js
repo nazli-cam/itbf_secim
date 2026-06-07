9d4d786c5d89abbcf471828cf92ba48dccbdac92
nazli-cam <nazli@e2.vc>
2026-06-07 12:31:05 +0300

    Update vote.js

--- src/routes/vote.js
+++ src/routes/vote.js
@@ -1,4 +1,4 @@
-const express = require('express');
+-const express = require('express');
 const router = express.Router();
 const queries = require('../db/queries');
 const voteService = require('../services/voteService');
